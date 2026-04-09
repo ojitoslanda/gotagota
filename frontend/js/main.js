ee1352e1f40c33a1d03edf6ed7a69e634526f34c
Gabriel Ojitos Landa <ojitoslanda@gmail.com>
2026-04-09 16:59:18 -0500

    asdasdasd

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,7 +2,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetch('http://localhost:8080/api/clientes')
         .then(response => response.json())
-        .then(
-            data => console.log(data)
-        );
+        .then(data => {
+            console.log('Datos:', data);
+            const elemento = document.getElementById("tabla-cliente")
+            elemento.innerHTML = JSON.stringify(data);
+            console.log(elemento)
+        })
 });
