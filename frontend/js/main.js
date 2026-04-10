d59b1165524377aaca55fb821c7ba510455abeac
Gabriel Ojitos Landa <ojitoslanda@gmail.com>
2026-04-10 16:47:38 -0500

    EWQF

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,15 +1,15 @@
 // EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,)
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('http://localhost:8080/api/clientes')
-        .then(response => response.json())
-        .then(data => {
-            // DOM -> <tbody id="table-cliente">
-            const elemento = document.getElementById("table-cliente")
-            for (let i = 0; i < data.length; i++) {
-                //data[i], muestra en forma de array
-                let cliente = data[i]
-                // alt + 96 
-                let fila = `
+  fetch("http://localhost:8080/api/clientes")
+    .then((response) => response.json())
+    .then((data) => {
+      // DOM -> <tbody id="table-cliente">
+      const elemento = document.getElementById("table-cliente");
+      for (let i = 0; i < data.length; i++) {
+        //data[i], muestra en forma de array
+        let cliente = data[i];
+        // alt + 96
+        let fila = `
                             <tr>
                             <td>${cliente.id}</td>
                             <td>${cliente.nombre}</td>
@@ -17,11 +17,19 @@ document.addEventListener("DOMContentLoaded", () => {
                             <td>${cliente.dni}</td>
                             <td>${cliente.telefono}</td>
                             <td>${cliente.direccion}</td>
+                            <td> 
+                                <button class="btn btn-outline-primary me-2">
+                                    <i class="fas fa-edit"></i> Editar
+                                </button>
+                                <button class="btn btn-outline-danger">
+                                    <i class="fas fa-trash"></i> Eliminar
+                                </button>
+                            </td>
                             </tr>                
-                           `
-                elemento.innerHTML += fila  
-                      
-                //console.log(cliente) //Muestro los resultados en consola
-            }
-        })
+                           `;
+        elemento.innerHTML += fila;
+
+        //console.log(cliente) //Muestro los resultados en consola
+      }
+    });
 });
