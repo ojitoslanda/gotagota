053ba480229fba32d7e527c4a97f616c51ca94d3
Gabriel Ojitos Landa <ojitoslanda@gmail.com>
2026-04-10 15:17:24 -0500

    sdfsdf

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,10 +3,27 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch('http://localhost:8080/api/clientes')
         .then(response => response.json())
         .then(data => {
-            
-            //console.log('Datos:', data);
+            // DOM -> <tbody id="table-cliente">
             const elemento = document.getElementById("table-cliente")
-            elemento.innerHTML = JSON.stringify(data);
-            console.log(elemento)
+
+            for (let i = 0; i < data.length; i++) {
+                //data[i], muestra en forma de array
+                let cliente = data[i]
+                // alt + 96 
+                let fila = `
+                            <tr>
+                            <td>${cliente.id}</td>
+                            <td>${cliente.nombre}</td>
+                            <td>${cliente.apellido}</td>
+                            <td>${cliente.dni}</td>
+                            <td>${cliente.telefono}</td>
+                            <td>${cliente.direccion}</td>
+                            </tr>                
+                           `
+                console.log(cliente)
+            }
         })
+
+
+
 });
