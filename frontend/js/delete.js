6a2c323644c6c4767a309521f0c9eca52a85281e
Gabriel Ojitos Landa <ojitoslanda@gmail.com>
2026-04-11 15:58:43 -0500

    b

--- frontend/js/delete.js
+++
@@ -1,8 +0,0 @@
-// EVENTO DE CLICK EN JAVASCRIPT
-//Creamos una variable que almacene el DOM de ese elemento del boton
-document.addEventListener("click", function (e) {
-  const btnDelete = e.target.closest("#btnEliminar");
-  if (btnDelete) {
-    alert("Eliminando...");
-  }
-});
