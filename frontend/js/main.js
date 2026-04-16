879ae081b6a2302acef82e1362ce5b08d548dcff
Gabriel Ojitos Landa <ojitoslanda@gmail.com>
2026-04-16 15:45:59 -0500

    sdasdasd

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -19,7 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
                             <td>${cliente.direccion}</td>
                             <td> 
                                 <button
-                                     
                                     class="btn btn-outline-primary me-2">
                                     <i class="fas fa-edit"></i> Editar
                                 </button>
@@ -32,27 +31,43 @@ document.addEventListener("DOMContentLoaded", () => {
         elemento.innerHTML += fila;
       }
     });
-
 });
 
 // EVENTO DE CLICK EN JAVASCRIPT
 //Creamos una variable que almacene el DOM de ese elemento del boton
 document.addEventListener("click", function (e) {
   const btnDelete = e.target.closest("#btnEliminar");
-    if (btnDelete) { //TRUE o 1
-        alert("Eliminando...");
-        const id = btnDelete.dataset.idcliente;
-        //console.log(id) para en consola que ID es nada mas
-        //fetch("http://localhost:8080/api/clientes/"+id, {
-        fetch(`http://localhost:8080/api/clientes/${id}`, {
-          method: 'DELETE'
-        })
-        .then(response => {
-            if (response.ok) {
-              alert('Cliente eliminado correctamente');
-              location.reload(); // Recargar la página para reflejar los cambios
-            } else {
-              alert('Error al eliminar el cliente');
-        }})
-    }
+  if (btnDelete) {
+    //TRUE o 1
+    alert("Eliminando...");
+    const id = btnDelete.dataset.idcliente;
+    //console.log(id) para en consola que ID es nada mas
+    //fetch("http://localhost:8080/api/clientes/"+id, {
+    fetch(`http://localhost:8080/api/clientes/${id}`, {
+      method: "DELETE",
+    }).then((response) => {
+      if (response.ok) {
+        alert("Cliente eliminado correctamente");
+        location.reload(); // Recargar la página para reflejar los cambios
+      } else {
+        alert("Error al eliminar el cliente");
+      }
+    });
+  }
 });
+
+// CREAMOS UN FUNCION BASICA
+function guardarCliente() {
+  const nombre = document.getElementById("c_nombre");
+  const apellido = document.getElementById("c_apellido");
+  const dni = document.getElementById("c_dni");
+  const telefono = document.getElementById("c_telefono");
+  const direccion = document.getElementById("c_direccion");
+    fetch("http://localhost:8080/api/clientes", {
+      method: "POST",
+      headers: {"Content-Type":"application/json"},
+      body: JSON.stringify({nombre,apellido,dni,telefono,direccion}) 
+    }).then((response) => {
+        console.log(response)
+    });
+}
